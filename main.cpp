0cf747d9c9ac4e1da03eefba4cf74feeafa31caf
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-09-27 17:19:51 +0900

    06

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-   int x;
-   printf("input year:");
-   scanf("%i",&x);
-   if (x%4==0 && x%100!=0 || x%400==0)
-       printf("%i is leap year!\n",x);
-   else
-       printf("%i is not leap year!\n",x);
+	int x;
+	printf("input the second:");
+	scanf("%i",&x);
+	printf("The time for %i second is: %i:%i:%i",x,x/3600,(x%3600)/60,x%60);
+	
     return 0;
 }
