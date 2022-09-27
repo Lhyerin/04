7b8ed90bf0bbc491b6b29d469f37c681ec808021
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-09-27 16:49:19 +0900

    01

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int a;
-	printf("input the second:");
-	scanf("%i",&a);
-	printf("%i the second is %i : %i\n",a,a/60,a%60);
+   int x, y, z, m;
+   int a, b, c;
+   x=1;
+   y=3; z=5;
+   a=3;
+   b=4;
+   c=5;
+   y=a*x*x+b*x+c;
+   m=(x+y+z)/3;
+   printf("y=%d, m=%d", y, m);
 	return 0;
 }
